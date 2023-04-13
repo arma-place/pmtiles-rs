87fed09beadd9c9b8d80c3c9bce81ed5d4466125
DerZade <DerZade@gmail.com>
2023-04-14 01:09:22 +0200

    feat: :sparkles: Added async methods to directory

--- src/directory.rs
+++ src/directory.rs
@@ -1,9 +1,11 @@
-use integer_encoding::{VarIntReader, VarIntWriter};
+use duplicate::duplicate_item;
+use futures::{AsyncRead, AsyncReadExt, AsyncWrite};
+use integer_encoding::{VarIntAsyncReader, VarIntAsyncWriter, VarIntReader, VarIntWriter};
 use std::io::{Read, Result, Write};
 use std::ops::{Index, IndexMut, Range};
 use std::slice::{Iter, SliceIndex};
 
-use crate::util::{compress, decompress};
+use crate::util::{compress, compress_async, decompress, decompress_async};
 use crate::Compression;
 
 /// A structure representing a directory entry.
@@ -48,7 +50,7 @@ impl Entry {
 /// A directory holds an arbitrary amount of [`Entry`]. You can use [`len`](Self::len), [`is_empty`](Self::is_empty) and
 /// [`iter`](Self::iter) to obtain information about that list of entries.
 ///
-/// Use [`from_reader`](Self::from_reader) and [`to_writer`](Self::to_writer) to read and write the directory from / to bytes.
+/// Use [`from_reader`](Self::from_reader) and [`to_writer`](Self::to_writer) or their respective asynchronous versions ([`from_async_reader`](Self::from_async_reader) and [`to_async_writer`](Self::to_async_writer)) to read and write the directory from / to bytes.
 #[derive(Debug, PartialEq, Eq, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(transparent))]
@@ -73,7 +75,111 @@ impl Directory {
     pub fn iter(&self) -> Iter<'_, Entry> {
         self.entries.iter()
     }
+}
 
+impl Directory {
+    #[duplicate_item(
+        fn_name                  input_traits                                     decompress(compression, binding)              read_varint(type, reader)                  async;
+        [from_reader_impl]       [impl Read]                                      [decompress(compression, &mut binding)]       [reader.read_varint::<type>()]             [];
+        [from_async_reader_impl] [(impl AsyncRead + Unpin + Send + AsyncReadExt)] [decompress_async(compression, &mut binding)] [reader.read_varint_async::<type>().await] [async];
+    )]
+    #[allow(clippy::needless_range_loop)]
+    async fn fn_name(
+        input: &mut input_traits,
+        length: u64,
+        compression: Compression,
+    ) -> Result<Self> {
+        let mut binding = input.take(length);
+        let mut reader = decompress([compression], [binding])?;
+
+        let num_entries = read_varint([usize], [reader])?;
+
+        let mut entries = Vec::<Entry>::with_capacity(num_entries);
+
+        // read tile_id
+        let mut last_id = 0u64;
+        for _ in 0..num_entries {
+            let tmp = read_varint([u64], [reader])?;
+
+            last_id += tmp;
+            entries.push(Entry {
+                tile_id: last_id,
+                length: 0,
+                offset: 0,
+                run_length: 0,
+            });
+        }
+
+        // read run_length
+        for i in 0..num_entries {
+            entries[i].run_length = read_varint([_], [reader])?;
+        }
+
+        // read length
+        for i in 0..num_entries {
+            entries[i].length = read_varint([_], [reader])?;
+        }
+
+        // read offset
+        for i in 0..num_entries {
+            let val = read_varint([u64], [reader])?;
+
+            entries[i].offset = if i > 0 && val == 0 {
+                entries[i - 1].offset + u64::from(entries[i - 1].length)
+            } else {
+                val - 1
+            };
+        }
+
+        Ok(Self { entries })
+    }
+
+    #[duplicate_item(
+        fn_name                input_traits                       compress(compression, output)         write_varint(writer, value)              async;
+        [to_writer_impl]       [impl Write]                       [compress(compression, output)]       [writer.write_varint(value)]             [];
+        [to_async_writer_impl] [(impl AsyncWrite + Unpin + Send)] [compress_async(compression, output)] [writer.write_varint_async(value).await] [async];
+    )]
+    async fn fn_name(&self, output: &mut input_traits, compression: Compression) -> Result<()> {
+        let mut writer = compress([compression], [output])?;
+
+        write_varint([writer], [self.entries.len()])?;
+
+        // write tile_id
+        let mut last_id = 0u64;
+        for entry in &self.entries {
+            write_varint([writer], [entry.tile_id - last_id])?;
+            last_id = entry.tile_id;
+        }
+
+        // write run_length
+        for entry in &self.entries {
+            write_varint([writer], [entry.run_length])?;
+        }
+
+        // write length
+        for entry in &self.entries {
+            write_varint([writer], [entry.length])?;
+        }
+
+        // write offset
+        let mut next_byte = 0u64;
+        for (index, entry) in self.entries.iter().enumerate() {
+            let val = if index > 0 && entry.offset == next_byte {
+                0
+            } else {
+                entry.offset + 1
+            };
+
+            write_varint([writer], [val])?;
+
+            next_byte = entry.offset + u64::from(entry.length);
+        }
+
+        Ok(())
+    }
+}
+
+impl Directory {
     /// Reads a directory from a [`std::io::Read`] and returns it.
     ///
     /// # Arguments
@@ -95,55 +201,45 @@ impl Directory {
     ///
     /// let directory = Directory::from_reader(&mut reader, 246, Compression::GZip).unwrap();
     /// ```
-    #[allow(clippy::needless_range_loop)]
     pub fn from_reader(
         input: &mut impl Read,
         length: u64,
         compression: Compression,
     ) -> Result<Self> {
-        let mut binding = input.take(length);
-        let mut reader = decompress(compression, &mut binding)?;
+        Self::from_reader_impl(input, length, compression)
+    }
 
-        let num_entries = reader.read_varint::<usize>()?;
-
-        let mut entries = Vec::<Entry>::with_capacity(num_entries);
-
-        // read tile_id
-        let mut last_id = 0u64;
-        for _ in 0..num_entries {
-            let tmp = reader.read_varint::<u64>()?;
-
-            last_id += tmp;
-            entries.push(Entry {
-                tile_id: last_id,
-                length: 0,
-                offset: 0,
-                run_length: 0,
-            });
-        }
-
-        // read run_length
-        for i in 0..num_entries {
-            entries[i].run_length = reader.read_varint()?;
-        }
-
-        // read length
-        for i in 0..num_entries {
-            entries[i].length = reader.read_varint()?;
-        }
-
-        // read offset
-        for i in 0..num_entries {
-            let val = reader.read_varint::<u64>()?;
-
-            entries[i].offset = if i > 0 && val == 0 {
-                entries[i - 1].offset + u64::from(entries[i - 1].length)
-            } else {
-                val - 1
-            };
-        }
-
-        Ok(Self { entries })
+    /// Async version of [`from_reader`](Self::from_reader).
+    ///
+    /// Reads a directory from a [`futures::io::AsyncRead`] and returns it.
+    ///
+    /// # Arguments
+    /// * `input` - Reader including directory bytes
+    /// * `length` - Length of the directory (in bytes)
+    /// * `compression` - Compression of the  directory
+    ///
+    /// # Errors
+    /// Will return [`Err`] if `compression` is set to [`Compression::Unknown`], the data is not compressed correctly
+    /// according to `compression` or an I/O error occurred while reading from `input`.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use pmtiles2::{Directory, Compression};
+    /// # use futures::io::{AsyncReadExt, AsyncSeekExt, SeekFrom};
+    /// # tokio_test::block_on(async {
+    /// let bytes = include_bytes!("../test/stamen_toner(raster)CC-BY+ODbL_z3.pmtiles");
+    /// let mut reader = futures::io::Cursor::new(bytes);
+    /// reader.seek(SeekFrom::Start(127)).await.unwrap();
+    ///
+    /// let directory = Directory::from_async_reader(&mut reader, 246, Compression::GZip).await.unwrap();
+    /// # })
+    /// ```
+    pub async fn from_async_reader(
+        input: &mut (impl AsyncRead + Unpin + Send + AsyncReadExt),
+        length: u64,
+        compression: Compression,
+    ) -> Result<Self> {
+        Self::from_async_reader_impl(input, length, compression).await
     }
 
     /// Writes the directory to a [`std::io::Write`].
@@ -166,42 +262,38 @@ impl Directory {
     /// directory.to_writer(&mut output, Compression::GZip).unwrap();
     /// ```
     pub fn to_writer(&self, output: &mut impl Write, compression: Compression) -> Result<()> {
-        let mut writer = compress(compression, output)?;
+        self.to_writer_impl(output, compression)
+    }
 
-        writer.write_varint(self.entries.len())?;
-
-        // write tile_id
-        let mut last_id = 0u64;
-        for entry in &self.entries {
-            writer.write_varint(entry.tile_id - last_id)?;
-            last_id = entry.tile_id;
-        }
-
-        // write run_length
-        for entry in &self.entries {
-            writer.write_varint(entry.run_length)?;
-        }
-
-        // write length
-        for entry in &self.entries {
-            writer.write_varint(entry.length)?;
-        }
-
-        // write offset
-        let mut next_byte = 0u64;
-        for (index, entry) in self.entries.iter().enumerate() {
-            let val = if index > 0 && entry.offset == next_byte {
-                0
-            } else {
-                entry.offset + 1
-            };
-
-            writer.write_varint(val)?;
-
-            next_byte = entry.offset + u64::from(entry.length);
-        }
-
-        Ok(())
+    /// Async version of [`to_writer`](Self::to_writer).
+    ///
+    /// Writes the directory to a [`futures::io::AsyncWrite`].
+    ///
+    /// # Arguments
+    /// * `output` - Writer to write directory to
+    /// * `compression` - Compression to use
+    ///
+    /// # Errors
+    /// Will return [`Err`] if `compression` is set to [`Compression::Unknown`] or an I/O
+    /// error occurred while writing to `output`.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use pmtiles2::{Directory, Compression};
+    /// # tokio_test::block_on(async {
+    /// let directory: Directory = Vec::new().into();
+    ///
+    /// let mut output = futures::io::Cursor::new(Vec::<u8>::new());
+    ///
+    /// directory.to_async_writer(&mut output, Compression::GZip).await.unwrap();
+    /// # })
+    /// ```
+    pub async fn to_async_writer(
+        &self,
+        output: &mut (impl AsyncWrite + Unpin + Send),
+        compression: Compression,
+    ) -> Result<()> {
+        self.to_async_writer_impl(output, compression).await
     }
 }
 
